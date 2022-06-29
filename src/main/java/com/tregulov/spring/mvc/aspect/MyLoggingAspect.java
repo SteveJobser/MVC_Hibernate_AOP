64dc975268938ae3f4f6e4d516f6808f85a45baa
SteveJobser <62641418+SteveJobser@users.noreply.github.com>
2022-06-29 17:52:15 +0600

    Update MyLoggingAspect.java

--- src/main/java/com/tregulov/spring/mvc/aspect/MyLoggingAspect.java
+++ src/main/java/com/tregulov/spring/mvc/aspect/MyLoggingAspect.java
@@ -1,4 +1,14 @@
 package com.tregulov.spring.mvc.aspect;
 
+import org.aspectj.lang.annotation.Aspect;
+
+@Component
+@Aspect
+@Order(1)
 public class MyLoggingAspect {
+  
+  @Before("execution(public List<Employee> getAllEmployees())")
+	static void loggingGetAllEmployee(args) {
+	    System.out.println("Логируем данные в БД");
+	}
 }
